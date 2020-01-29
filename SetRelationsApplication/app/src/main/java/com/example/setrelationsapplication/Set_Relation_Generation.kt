619517152257f7a597666a14593c5769ab70fa05
ElliottJBolt <44170952+ElliottJBolt@users.noreply.github.com>
2020-01-29 18:59:34 +0000

    Actual Reflexive relation
    Made them actually reflexive

--- SetRelationsApplication/app/src/main/java/com/example/setrelationsapplication/Set_Relation_Generation.kt
+++ SetRelationsApplication/app/src/main/java/com/example/setrelationsapplication/Set_Relation_Generation.kt
@@ -4,35 +4,33 @@ import android.util.Log.d
 import kotlin.random.Random
 
 object Set_Relation_Generation {
-    fun setGenerator() : MutableList<Int>  {
-        val setLength = Random.nextInt(4, 8)
+    fun setGenerator(): MutableList<Int> {
+        val setLength = Random.nextInt(4, 6)
         val setValues: MutableList<Int> = mutableListOf<Int>()
         var x = 2
-        do{
+        do {
             var nextValue = Random.nextInt(0, 9)
-            d("Elliott"," Original value $nextValue")
-            var uniqueValue = checkRepeated(setValues,nextValue)
+            var uniqueValue = checkRepeated(setValues, nextValue)
             setValues.add(uniqueValue)
             x++
 
-        }while (x <= setLength + 1)
+        } while (x <= setLength + 1)
 
         return setValues
 
     }
 
-    fun checkRepeated(values: MutableList<Int>,nextValue: Int): Int {
+    fun checkRepeated(values: MutableList<Int>, nextValue: Int): Int {
         var nextValue = nextValue
-            if (values.contains(nextValue)){
-                do{
-                    nextValue = Random.nextInt(0, 9)
-                    d("Elliott"," New value $nextValue")
-                    checkRepeated(values,nextValue)
+        if (values.contains(nextValue)) {
+            do {
+                nextValue = Random.nextInt(0, 9)
+                checkRepeated(values, nextValue)
 
-                }while (values.contains(nextValue))
-            }else{
-                return nextValue
-            }
+            } while (values.contains(nextValue))
+        } else {
+            return nextValue
+        }
         return nextValue
 
     }
@@ -40,52 +38,83 @@ object Set_Relation_Generation {
     fun relationGenerator(values: MutableList<Int>): MutableList<Int> {
         var lengthOfSet = values.size
         var createdRelation: MutableList<Int> = mutableListOf<Int>()
-        var noOfRelationPairs = Random.nextInt(3,7)
+        var noOfRelationPairs = Random.nextInt(3, 7)
         var y = 0
 
         do {
-            var number = Random.nextInt(0,lengthOfSet)
+            var number = Random.nextInt(0, lengthOfSet)
             var orderedPairValue = values.elementAt(number)
             createdRelation.add(orderedPairValue)
             y++
 
-        }while (y < noOfRelationPairs * 2)
+        } while (y < noOfRelationPairs * 2)
 
-        return  createdRelation
+        return createdRelation
     }
 
     fun reflexive(values: MutableList<Int>): MutableList<Int> {
         var lengthOfSet = values.size
-        var refelexiveRelation: MutableList<Int> = mutableListOf<Int>()
+        var reflexiveRelation: MutableList<Int> = mutableListOf<Int>()
 
-        var noOfRelationPairs = Random.nextInt(3,7)
+        var noOfRelationPairs = Random.nextInt(lengthOfSet, 8)
+        d("Elliott", "no of relation pairs: $noOfRelationPairs")
 
-        var noOfNumbers = 2*noOfRelationPairs
-
-
+        var x = 0
         var y = 0
+        var number: Int
+        var actualValue: Int
 
-        var reflexiveNumber = Random.nextInt(0,lengthOfSet)
-
+        var allReflexiveNum: MutableList<Int> = mutableListOf<Int>()
 
         do {
-            var number = Random.nextInt(0,lengthOfSet)
+            allReflexiveNum.add(values.elementAt(x))
+            d("Elliott", "$allReflexiveNum")
+            x++
+        } while (x < values.size)
+
+        var allReflexNumLength = allReflexiveNum.size
+
+        allReflexiveNum.shuffle()
+
+        val randomNum = Random.nextInt(1,2)
+
+        if (randomNum.rem(2) == 1){
+            number = Random.nextInt(0, lengthOfSet)
+            actualValue = values.elementAt(number)
+            reflexiveRelation.add(actualValue)
+            number = Random.nextInt(0, lengthOfSet)
+            actualValue = values.elementAt(number)
+            reflexiveRelation.add(actualValue)
+        }
+
+        do {
+            var reflexiveNumber = allReflexiveNum.first()
             var orderedPairValue: Int
 
-            if (reflexiveNumber.rem(2) == 1 && y == reflexiveNumber || reflexiveNumber.rem(2) == 1 && y == reflexiveNumber - 1 ){
-                orderedPairValue = values.elementAt(reflexiveNumber)
-            }else if(reflexiveNumber.rem(2) == 0 && y == reflexiveNumber || reflexiveNumber.rem(2) == 0  && y == reflexiveNumber + 1){
-                orderedPairValue = values.elementAt(reflexiveNumber)
-            }else{
-                orderedPairValue = values.elementAt(number)
-            }
-            refelexiveRelation.add(orderedPairValue)
-            
+            orderedPairValue = reflexiveNumber
+            reflexiveRelation.add(orderedPairValue)
+            reflexiveRelation.add(orderedPairValue)
+
+            allReflexiveNum.removeAt(0)
             y++
 
-        }while (y < noOfNumbers)
 
-        return refelexiveRelation
+
+        } while (y < allReflexNumLength)
+
+        if (allReflexNumLength < noOfRelationPairs) {
+
+            number = Random.nextInt(0, lengthOfSet)
+            actualValue = values.elementAt(number)
+            reflexiveRelation.add(actualValue)
+
+            number = Random.nextInt(0, lengthOfSet)
+            actualValue = values.elementAt(number)
+            reflexiveRelation.add(actualValue)
+
+        }
+
+        return reflexiveRelation
 
     }
 
